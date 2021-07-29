373959f6236cbab0367df0a135a7a3bdff41505b
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-07-29 23:26:30 +0900

    study: mongoose 스키마 작성

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://thdwlsgus0:1234@bolierplate.hp0z2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(()=> console.log('MongoDB connected..'))
+  .catch(err => console.log(err));
+
+
+
 app.get('/', (req, res) => res.send('Hello World'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
