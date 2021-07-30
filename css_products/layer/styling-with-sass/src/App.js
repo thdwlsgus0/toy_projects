6012b448ea752fb896983f2c0c2e69e7994c2288
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-07-31 03:35:41 +0900

    study: node.js mongoose schema 생성

---
+++ css_products/layer/styling-with-sass/src/App.js
@@ -0,0 +1,32 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="lessor_review type_documents">
+      <div className="lessor_review_description">
+          <div className="lessor_description_title">심사중</div>
+          <div className="lessor_description_item">
+              <strong className="lessor_item_title">목동 아이파크 위브</strong>
+              <p className="lessor_item_text">C동 638호 외 5건</p>
+          </div>
+      </div>
+      <div className="lessor_review_description">
+          <div className="lessor_description_title">심사중</div>
+          <div className="lessor_description_item">
+              <strong className="lessor_item_title">대림 아크로텔</strong>
+              <p className="lessor_item_text">102동 1043호 외 9건</p>
+          </div>
+      </div>
+      <div className="lessor_review_description">
+          <div className="lessor_description_title">심사중</div>
+          <div className="lessor_description_item">
+              <strong className="lessor_item_title">래미안블레스티지</strong>
+              <p className="lessor_item_text">1102동 102호 외 3건</p>
+          </div>
+      </div> 
+    </div>
+
+  );
+}
+
+export default App;
