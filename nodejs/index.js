2668467a0980d0167a2e712a7a485363bc5e67b9
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-08-08 00:23:01 +0900

    study: nodemon 설치

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const {User} = require('./models/User');
+const config = require('./config/key');
+
+// application/x-www-form-urlencoded 데이터를 가져오는 것
+app.use(bodyParser.urlencoded({extended:true}));
+
+// application/json을 가져오기 위해서 사용
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://thdwlsgus0:1234@bolierplate.hp0z2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB connected..'))
   .catch(err => console.log(err));
 
-
-
 app.get('/', (req, res) => res.send('Hello World'));
+
+app.post('/register', (req, res) => {
+	// 회원 가입할 때 필요한 정보들을 client에서 가져오면
+	// 그것들을 데이터 베이스에 넣어줍니다.
+	
+	const user = new User(req.body);
+	
+	user.save((err, userInfo) => {
+		if(err) return res.json({ success: false, err});
+		return res.status(200).json({
+			success: true
+		})
+	});
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
