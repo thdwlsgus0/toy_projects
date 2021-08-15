c48718e5f4664a0b30ecec99f270d548e0ca0ef3
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-08-15 22:58:07 +0900

    feat: 로그인 라우터 구현

--- nodejs/models/User.js
+++ nodejs/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -37,6 +39,35 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+userSchema.pre('save', function(next) {
+  
+	let user = this;
+
+	if(user.isModified('password')) {
+		// 비밀번호를 암호화 시킨다.
+		bcrypt.genSalt(saltRounds, function(err, salt) {
+			if(err) return next(err);
+
+			bcrypt.hash(user.password, salt, function(err, hash) {
+				if(err) return next(err);
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		next();
+	}
+});
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+
+	// plainPassword 1234567 암호화된 비밀번호
+	bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+		if(err) return cb(err),
+		cb(null, isMatch)
+	});	
+}
 
 const User = mongoose.model('User', userSchema);
 
